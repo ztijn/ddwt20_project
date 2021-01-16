28b1f171613ca35cead93c9ca6136e56b0c3ebfe
ztijn <stijnwijnen1997@hotmail.com>
2021-01-16 19:36:44 +0100

    removed login button in card for users that already are logged in and replaced it with a button to room overview

--- views/cards_login.php
+++ views/cards_login.php
@@ -6,8 +6,7 @@
     <div class="card-body">
         <p class="count">Total rooms available:</p>
         <h2><?= $nbr_rooms_available ?></h2>
-        <p>Login to you account to see the available rooms</p>
-        <a href="/ddwt20_project/login/" class="btn btn-primary">Login</a>
+        <a href="/ddwt20_project/overview/" class="btn btn-primary">View Rooms</a>
     </div>
 </div>
 <p></p>
